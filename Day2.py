f1dd32e96d3068b6705b3988687ff728b3e23b89
Josh Ethridge <jethridge@kmbs.konicaminolta.us>
2018-01-23 13:58:47 -0500

    Day 2 Part 2

--- Day2.py
+++ Day2.py
@@ -23,6 +23,31 @@ def calc(data):
 		numbers.append(keypad[index[1]][index[0]])
 	return codify(numbers)
 
+def calc2(data):
+	keypad = [[0, 0, 1, 0, 0],
+			  [0, 2, 3, 4, 0],
+			  [5, 6, 7, 8, 9],
+			  [0, 'A', 'B', 'C', 0],
+			  [0, 0, 'D', 0, 0]]
+	index = [0, 2]
+	numbers = ''
+	for i in data:
+		for j in i:
+			if j == 'U':
+				if index[1] > 0 and keypad[index[1] - 1][index[0]] != 0:
+					index[1] -= 1
+			elif j == 'D':
+				if index[1] < len(keypad[0]) - 1 and keypad[index[1] + 1][index[0]] != 0:
+					index[1] += 1
+			elif j == 'L':
+				if index[0] > 0 and keypad[index[1]][index[0] - 1] != 0:
+					index[0] -= 1
+			elif j == 'R':
+				if index[0] < len(keypad) - 1 and keypad[index[1]][index[0] + 1] != 0:
+					index[0] += 1
+		numbers += str(keypad[index[1]][index[0]])
+	return numbers
+
 def codify(l):
 	assert len(l) > 0
 	s = ''.join(map(str, l))
@@ -35,6 +60,7 @@ def load(path):
 			data.append(line)
 	return data
 
+
 class TestDay2(unittest.TestCase):
 
 	def test1(self):
@@ -45,9 +71,13 @@ class TestDay2(unittest.TestCase):
 		data = [1, 9, 8, 5]
 		self.assertEqual(codify(data), 1985)
 
+	def test3(self):
+		data = load('Day2Test.txt')
+		self.assertEqual(calc2(data), '5DB3')
+
 if __name__ == '__main__':
 	#unittest.main()
 	# Part 1: 61529
 	print(calc(load('Day2.txt')))
-	# Part 2:
-
+	# Part 2: C2C28
+	print(calc2(load('Day2.txt')))
