61f2171dc6c6d4a9d4af1b70a258650374bc23fd
Josh Ethridge <joshua.ethridge13@gmail.com>
2018-02-06 16:03:58 -0500

    Day 9 Part 2 and Day 10 Part 1 WIP

--- Day9.py
+++ Day9.py
@@ -1,4 +1,5 @@
 import unittest
+import re
 
 def calc(line):
 	decompressed = ''
@@ -7,41 +8,67 @@ def calc(line):
 		decompressed += decompress(i)
 	return len(decompressed)
 
+def calc2test(line):
+	while '(' in line:
+		decompressed = ''
+		lines = breakIntoSegments(line)
+		for i in lines:
+			decompressed += decompress(i)
+		line = decompressed
+	print(decompressed)
+	return len(decompressed)
+
 def calc2(line):
 	# TODO
 	s = 0
 	lines = breakIntoSegments(line)
 	for i in lines:
+		print(i)
 		if ')' not in i:
 			s += len(i)
 		elif i.find(')') == i.rfind(')'):
-			index = i.find('(')
-			endIndex = i.find(')')
-			marker = i[index:endIndex+1]
+			marker = getFirstMarker(i)
 			length, repeat = getNumbersFromMarker(marker)
 			s += length * repeat
-			s += len(i[:index])
+			s += len(i[:i.find('(')])
 		else:
+			# TODO Bugged for n > 2 nested markers
 			nested = i
 			while nested.find(')') != nested.rfind(')'):
-				index = nested.find('(')
-				s += len(nested[:index])
-				endIndex = nested.find(')')
-				marker = nested[index:endIndex+1]
-				_, repeat = getNumbersFromMarker(marker)
-				nested = nested[endIndex+1:]
-				secondIndex = nested.find('(')
-				secondEndIndex = nested.find(')')
-				secondMarker = nested[secondIndex:secondEndIndex+1]
-				_, secondRepeat = getNumbersFromMarker(secondMarker)
-				newRepeat = str(repeat * secondRepeat)
-				nested = nested[:nested.find('x') + 1] + newRepeat + nested[endIndex-1:]
-			index = nested.find('(')
-			endIndex = nested.find(')')
-			marker = nested[index:endIndex+1]
+				marker = getFirstMarker(nested)
+				# Account for not repeated characters before a marker
+				frontPadding = 0
+				frontPadding = len(nested[:nested.find(marker)])
+				s += frontPadding
+
+				# Get length and repeat count for lead marker
+				length, repeat = getNumbersFromMarker(marker)
+
+				nested = nested[nested.find(marker)+len(marker):]
+				# Get all markers within length
+				markers = re.findall(r'\(\d+x\d+\)', nested[:length])
+				newMarkers = {}
+				# Multiply repeat by repeat in first marker
+				lastMarker = None
+				for i in markers:
+					l, r = getNumbersFromMarker(i)
+					newRepeat = r * repeat
+					newMarkers[i] = '(' + str(l) + 'x' + str(newRepeat) + ')'
+					lastMarker = newMarkers[i]
+				print(newMarkers)
+				# Replace all old markers with their new counterparts
+				for i in newMarkers.keys():
+					nested = nested.replace(i, newMarkers[i])
+			marker = getFirstMarker(nested)
 			length, repeat = getNumbersFromMarker(marker)
 			s += length * repeat
 	return s
+
+def getFirstMarker(s):
+	index = s.find('(')
+	endIndex = s.find(')')
+	marker = s[index:endIndex+1]
+	return marker
 
 def decompress(s):
 	if '(' not in s:
@@ -129,6 +156,8 @@ class TestDay9(unittest.TestCase):
 		self.assertEqual(len(r), 3)
 
 	def test5(self):
+		t = '(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN'
+		self.assertEqual(calc2test(t), 445)
 		t = 'ADVENT'
 		self.assertEqual(calc2(t), 6)
 		t = 'A(1x5)BC'
